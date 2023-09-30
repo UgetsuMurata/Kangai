e90638e78fcf8fe45d57d3e605a401b1ddc9cc56
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-09-30 09:29:03 +0800

    Created necessary files

---
+++ app/src/main/java/com/example/kangai/Accounts/EditAccount.java
@@ -0,0 +1,16 @@
+package com.example.kangai.Accounts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kangai.R;
+
+public class EditAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accounts_edit);
+    }
+}
---
+++ app/src/main/java/com/example/kangai/Settings/Settings.java
@@ -0,0 +1,16 @@
+package com.example.kangai.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kangai.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_settings);
+    }
+}
