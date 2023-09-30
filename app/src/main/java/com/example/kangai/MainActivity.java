548be9727bea20a12907e734c867736ac8f8bf44
Kai Yaneza <92856337+yaneza-teamlight@users.noreply.github.com>
2023-09-30 09:21:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kangai/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kangai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
