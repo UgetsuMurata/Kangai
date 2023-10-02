61b743e1f3f0d88147d366f2bba23f732e4ff28b
Angelo Echano <grey12angelo@gmail.com>
2023-10-02 21:35:32 +0800

    added header to the dashboard and editAccount

--- app/src/main/java/com/example/kangai/Accounts/EditAccount.java
+++ app/src/main/java/com/example/kangai/Accounts/EditAccount.java
@@ -1,8 +1,11 @@
 package com.example.kangai.Accounts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 
 import com.example.kangai.R;
 
@@ -12,5 +15,17 @@ public class EditAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accounts_edit);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_header, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/kangai/Dashboard/Dashboard.java
+++ app/src/main/java/com/example/kangai/Dashboard/Dashboard.java
@@ -1,8 +1,10 @@
 package com.example.kangai.Dashboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.example.kangai.R;
 
@@ -12,5 +14,18 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_dashboard);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("");
+        toolbar.setSubtitle("");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_header, menu);
+        return true;
+    }
+
 }
