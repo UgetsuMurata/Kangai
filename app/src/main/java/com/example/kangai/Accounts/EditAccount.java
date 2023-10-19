97d39c3f73552f8ae7efb4959b85d9cb7ec0b30b
Asus <francyvells@gmail.com>
2023-10-19 16:53:35 +0800

    update signin/signup

--- app/src/main/java/com/example/kangai/Accounts/EditAccount.java
+++ app/src/main/java/com/example/kangai/Accounts/EditAccount.java
@@ -9,21 +9,35 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 import com.example.kangai.Dashboard.AddDevice;
 import com.example.kangai.Dashboard.Dashboard;
 import com.example.kangai.Helpers.ToolbarMenu;
 import com.example.kangai.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class EditAccount extends AppCompatActivity {
 
     LinearLayout home;
+    TextView idChangeProfile, DeleteAccount;
+    TextInputLayout usernameField, passwordField,phoneNumberField;
+    MaterialButton button,button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accounts_edit);
+
+        usernameField = findViewById(R.id.usernameField);
+        passwordField = findViewById(R.id.passwordField);
+        phoneNumberField = findViewById(R.id.phoneNumberField);
+        idChangeProfile = findViewById(R.id.idChangeProfile);
+        DeleteAccount = findViewById(R.id.DeleteAccount);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         home = findViewById(R.id.home);
@@ -37,6 +51,9 @@ public class EditAccount extends AppCompatActivity {
         });
         toolbar.setTitle("");
         toolbar.setSubtitle("");
+
+
+
     }
 
     @Override
