f6c0a74801d5ee8eeba42ba1ee7ebd7fc5d24b1d
Angelo Echano <grey12angelo@gmail.com>
2023-11-04 03:54:47 +0800

    Update Edit Account

--- app/src/main/java/com/example/kangai/Accounts/EditAccount.java
+++ app/src/main/java/com/example/kangai/Accounts/EditAccount.java
@@ -5,19 +5,27 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.kangai.Dashboard.AddDevice;
 import com.example.kangai.Dashboard.Dashboard;
+import com.example.kangai.Firebase.FirebaseData;
 import com.example.kangai.Helpers.ToolbarMenu;
 import com.example.kangai.R;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class EditAccount extends AppCompatActivity {
 
@@ -52,6 +60,60 @@ public class EditAccount extends AppCompatActivity {
         toolbar.setTitle("");
         toolbar.setSubtitle("");
 
+        button2.setOnClickListener(view -> EditButton());
+
+    }
+
+    private void ShowUserData(){
+
+        FirebaseData fd = new FirebaseData();
+
+
+    }
+
+
+    private void EditButton(){
+        String username = Objects.requireNonNull(usernameField.getEditText()).getText().toString().trim();
+        String password = Objects.requireNonNull(passwordField.getEditText()).getText().toString().trim();
+        String number = Objects.requireNonNull(phoneNumberField.getEditText()).getText().toString().trim();
+
+        usernameField.setError("");
+        passwordField.setError("");
+        phoneNumberField.setError("");
+
+        FirebaseData fd = new FirebaseData();
+        fd.retrieveData(this, "ExistingUsernames/", new FirebaseData.FirebaseDataCallback() {
+            @Override
+            public void onDataReceived(DataSnapshot dataSnapshot) {
+                boolean valid = true;
+                String key = null;
+                for (DataSnapshot usernames : dataSnapshot.getChildren()) {
+                    if (usernames.getValue().equals(username)){
+                        valid = false;
+                        usernameField.setError("Username Already Exists.");
+                    }else{
+                        key = usernames.getKey();
+                        break;
+                    }
+                }
+
+                if (valid){
+                    fd.updateValue("ExistingUsernames/"+key, username);
+
+                    Map<String, Object> acctCred = new HashMap<>();
+
+                    acctCred.put("Username", username);
+                    acctCred.put("Password", password);
+                    acctCred.put("Image", "NULL");
+                    acctCred.put("Phone", number);
+
+                    fd.updateValues("Users/"+key+"/AcctCredentials/", acctCred);
+                    Toast.makeText(EditAccount.this, "Edit Succesfully", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
 
 
     }
